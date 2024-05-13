72f9fe6e62bf8bebb7a89d5ba0a66866829d6941
fgyeruhdijo <gs1marijche@hotmail.com>
2024-05-13 07:28:25 +0700

    feat

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-function subarraySum(nums, k) {
-  const map = new Map();
-  map.set(0, 1);
-  let count = 0;
-  let sum = 0;
-  for (const num of nums) {
-    sum += num;
-    if (map.has(sum - k)) {
-      count += map.get(sum - k);
-    }
-    map.set(sum, (map.get(sum) || 0) + 1);
-  }
-  return count;
+function sayHi() {
+  console.log("Hi!");
 }
+
+setTimeout(sayHi, 2000); // Run sayHi after 2 seconds
