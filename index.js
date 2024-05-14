cf2ee9fcff76888771d5a7237deb1e17863cad4f
fgyeruhdijo <gs1marijche@hotmail.com>
2024-05-14 08:33:24 +0700

    feat task

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-function sayHi() {
-  console.log("Hi!");
+function longestPalindrome(s) {
+  let longest = "";
+  for (let i = 0; i < s.length; i++) {
+    const oddPalindrome = expandAroundCenter(s, i, i);
+    const evenPalindrome = expandAroundCenter(s, i, i + 1);
+    const currentLongest =
+      oddPalindrome.length > evenPalindrome.length
+        ? oddPalindrome
+        : evenPalindrome;
+    if (currentLongest.length > longest.length) longest = currentLongest;
+  }
+  return longest;
 }
-
-setTimeout(sayHi, 2000); // Run sayHi after 2 seconds
+function expandAroundCenter(s, left, right) {
+  while (left >= 0 && right < s.length && s[left] === s[right]) {
+    left--;
+    right++;
+  }
+  return s.slice(left + 1, right);
+}
